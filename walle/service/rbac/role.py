1171c25133406c106fb6f72e1d7b1b819a80ce2f
meolu <861459320@qq.com>
2018-11-04 23:53:23 +0800

    add role

---
+++ walle/service/rbac/role.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    walle-web
+
+    :copyright: © 2015-2017 walle-web.io
+    :created time: 2018-11-04 22:08:28
+    :author: wushuiyong@walle-web.io
+"""
+
+ACCESS_ROLE = {
+    '10': 'GUEST',
+    '20': 'REPORT',
+    '30': 'DEVELOPER',
+    '40': 'MASTER',
+    '50': 'OWNER',
+    '60': 'SUPER',
+}
+
+ROLE_ACCESS = {
+    'GUEST': '10',
+    'REPORT': '20',
+    'DEVELOPER': '30',
+    'MASTER': '40',
+    'OWNER': '50',
+    'SUPER': '60',
+}
